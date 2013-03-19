cc7c33dd0fb9417a1cdf228e69d0f56fb552ed70
Cin316 <joe13reich@gmail.com>
2013-03-19 18:51:31 -0400

    Added new methods and updated more code

--- src/com/nchat/server/CommandCenter.java
+++ src/com/nchat/server/CommandCenter.java
@@ -2,6 +2,22 @@ package com.nchat.server;
 
 public class CommandCenter {
 	
+	public static String analyzeMessage(UserNChatThread user, String chat){
+		String outputText = "";
+		if( !chat.equals("") ){
+			
+			if( chat.charAt(0) == '/' ){
+				outputText = executeCommand(user, chat);
+			}else{
+				
+				outputText = chat;
+				
+			}
+			
+		}
+		return outputText;
+	}
+	
 	public static String executeCommand(UserNChatThread user, String command){
 		return "Command Output.";
 	}
