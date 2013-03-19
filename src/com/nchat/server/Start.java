dbc76e60633f5add7eae783846cef0cc985d5a0b
Cin316 <joe13reich@gmail.com>
2013-03-18 21:07:02 -0400

    Created Start class to be executed.

---
+++ src/com/nchat/server/Start.java
@@ -0,0 +1,13 @@
+package com.nchat.server;
+
+public class Start {
+	
+	public static final int port = 12314;
+	
+	public static void main(String[] args){
+		
+		NChatServer chatServer = new NChatServer();
+		
+	}
+	
+}
