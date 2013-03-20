e20a1e9408b1a6fb2c9f6709b9eb2a86c10e3d69
Cin316 <joe13reich@gmail.com>
2013-03-20 08:42:28 -0400

    Added version number.

--- src/com/nchat/server/Start.java
+++ src/com/nchat/server/Start.java
@@ -3,9 +3,11 @@ package com.nchat.server;
 public class Start {
 	
 	public static final int port = 24565;
+	public static final String version = "v 1.0";
 	
 	public static void main(String[] args){
 		
+		System.out.println("NChat Server " + Start.version + ".");
 		NChatServer chatServer = new NChatServer();
 		
 	}
