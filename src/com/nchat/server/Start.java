92dfab1e91ae89a4b3363c5bd729b1e0b2f3ebb1
Cin316 <joe13reich@gmail.com>
2013-03-24 09:04:13 -0400

    Added ability to customize port with command line argument.

--- src/com/nchat/server/Start.java
+++ src/com/nchat/server/Start.java
@@ -2,10 +2,14 @@ package com.nchat.server;
 
 public class Start {
 	
-	public static final int port = 13132;
+	public static int port = 13132;
 	public static final String version = "v 1.0";
 	
 	public static void main(String[] args){
+		
+		if (args.length>=1){
+			port = Integer.parseInt(args[0]);
+		}
 		
 		System.out.println("NChat Server " + Start.version + ".");
 		NChatServer chatServer = new NChatServer();
